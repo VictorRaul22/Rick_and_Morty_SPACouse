55beb4f61088d504a527eb1708e30fe22ecddcc1
victorraul <victor-raul-22@hotmail.com>
2022-01-26 03:58:36 -0500

    fix header

--- src/templates/Header.js
+++ src/templates/Header.js
@@ -3,7 +3,7 @@ const Header=()=>{
     <div class="Header-main">
         <div>
             <h1>
-                <a href="/">
+                <a href="#">
                 100tifi.co
                 </a>
             </h1>
